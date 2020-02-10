ef95fa349925a3f272c962254d189e0b63f27966
Michael Grace <mgrace@verticalapps.com>
2020-02-10 10:29:27 -0500

    Initial Commit

---
+++ Elementary-Name-Proc.py
@@ -0,0 +1,14 @@
+namevar = str(input("Enter Name to Check: "))
+eval_string ='6'
+write_instances = int(input("How Many Times SHall I Write It: "))
+eval_chars = int(input("Which Character Would You Like to Know?: "))
+index_account = -1
+count = 0
+
+while (count < write_instances):
+    eval_string = eval_string + namevar
+    count = count + 1
+    # Print (count)
+
+print (eval_string [eval_chars + index_account])
+# Print (eval_chars)
